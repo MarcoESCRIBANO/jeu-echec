7683d674055a221e6be56afdfe28558292a6a376
Marco <marco.escribano@yncrea.fr>
2020-03-31 15:31:13 +0200

    Menu en phaser + début de la logique du jeu
    +Serveur

---
+++ assets/js/GameScene.js
@@ -0,0 +1,17 @@
+class GameScene extends Phaser.Scene {
+	constructor() {
+		super({ key: 'GameScene'})
+	}
+    
+    preload(){
+
+        console.log("gamescene preload");
+    }
+
+    create(){
+        console.log("gamescene");
+    }
+    
+    
+    
+}
---
+++ index.js
@@ -0,0 +1,36 @@
+const port = 8100;
+
+const express = require('express');
+//const moduleTest = require('./server_module/module');
+//const Test = require('./server_module/Class');
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/assets/'));
+
+/*app.get('/',function(req, res, next){
+    moduleTest.a();
+    res.sendFile(__dirname + '/assets/views/index.html');
+});*/
+
+app.get('/',(req, res, next)=>{
+    //let test = new Test();
+    //test.testHello();
+    res.sendFile(__dirname + '/assets/views/index.html');
+});
+
+/*io.sockets.on('connection', (socket) =>{
+    io.emit('Hello', 'A new connection on our website');
+    socket.emit('Hello', 'Hello to you');
+    socket.on('private massage', (from,msg) => {
+        console.log('I received a private massage by ' + from + 'saying: ' + msg);
+    });
+    socket.on('disconnect', () => {
+        io.emit('User disconnected');
+    });
+})*/
+
+server.listen(port);
+console.log('Serveur lancé!');
